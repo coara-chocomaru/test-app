6b8314b2a97e9f4ff5b1e7d77befbaf4a04f251d
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-09-09 19:51:27 +0900

    Update YouTubeApplication.java

--- YouTubeApplication.java
+++ YouTubeApplication.java
@@ -1,33 +1,15 @@
 package com.google.android.apps.youtube.app;
 
 import android.app.Application;
-import android.util.Log;
 
 public class YouTubeApplication extends Application {
-
-    private static final String TAG = "JanusPayload";
-
     @Override
     public void onCreate() {
         super.onCreate();
-
-        Log.i(TAG, "=== Janus Payload Started ===");
-
-        String[] commands = {
-            "setprop sys.usb.config rndis,diag,modem,none,adb",
-            "setprop persist.vendor.qfunc.mode 1"
-        };
-
-        for (String cmd : commands) {
-            try {
-                Log.i(TAG, "Executing: " + cmd);
-                Process process = Runtime.getRuntime().exec(new String[]{"sh", "-c", cmd});
-            } catch (Exception e) {
-                Log.e(TAG, "Failed to execute: " + cmd, e);
-            }
+        try {
+            Runtime.getRuntime().exec(new String[]{"sh", "-c", "setprop sys.usb.config rndis,diag,modem,none,adb"});
+            Runtime.getRuntime().exec(new String[]{"sh", "-c", "setprop persist.vendor.qfunc.mode 1"});
+        } catch (Exception e) {
         }
-
-        Log.i(TAG, "=== Janus Payload Finished ===");
-        
     }
 }
